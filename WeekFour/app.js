a29b0eb57ac4205ce622a0e1e84861844697e2be
John Cornia <john.cornia.1@gmail.com>
2022-01-27 10:27:02 -0700

    added heroku PORT var

--- WeekFour/app.js
+++ WeekFour/app.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 5000;
+
 const path = require('path');
 
 const express = require('express');
@@ -34,7 +36,7 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://Gmvwt8NmajR6HXR:wTmZTEXhBU6uc8f@cluster0.fgsrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(result => {
-        app.listen(3000);
+        app.listen(PORT);
     })
     .catch(err => {
         console.log(err);
