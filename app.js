152ebc34301b68c296998dd96b481c2a0dedfe89
John Cornia <john.cornia.1@gmail.com>
2022-01-31 13:26:43 -0700

    put db credentials into untracked file

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
+//make environment variables available throughout application
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -19,12 +23,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findById('5bab316ce0a7c75f783cb8a8')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -33,24 +37,24 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
+    .connect(
+        'mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fgsrg.mongodb.net/shop?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Max',
+                    email: 'max@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
         });
-        user.save();
-      }
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
     });
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
