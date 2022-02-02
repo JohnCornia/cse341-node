22020a7f658a282818257f0209544d990ee0ddc0
John Cornia <john.cornia.1@gmail.com>
2022-02-02 15:03:00 -0700

    bringing master up to speed

--- app.js
+++ app.js
@@ -1,59 +1,33 @@
 const path = require('path');
 
-const PORT = process.env.PORT || 5000
-
-const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
 
 const corsOptions = {
     origin: "https://<your_app_name>.herokuapp.com/",
     optionsSuccessStatus: 200
 };
 
-//make environment variables available throughout application
-const dotenv = require("dotenv");
-dotenv.config();
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Gmvwt8NmajR6HXR:wTmZTEXhBU6uc8f@cluster0.fgsrg.mongodb.net/shop?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
 
-const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fgsrg.mongodb.net/shop?retryWrites=true&w=majority`;
-
-const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
-});
-
-app.use(cors(corsOptions));
-
-const options = {
-    family: 4
-};
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-    session({
-        secret: 'my secret',
-        resave: false,
-        saveUnitialized: false,
-        store: store
-    })
-);
 
 app.use((req, res, next) => {
     User.findById('61fad515935ec6285d41b21a')
@@ -66,11 +40,18 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);
 
 app.use(errorController.get404);
 
-const MONGODB_URL = process.env.MONGODB_URL || MONGODB_URI;
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
 
 mongoose
     .connect(
@@ -81,7 +62,7 @@ mongoose
             if (!user) {
                 const user = new User({
                     name: 'John',
-                    email: 'cor13025@byui.edu.com',
+                    email: 'cor13025@byui.edu',
                     cart: {
                         items: []
                     }
