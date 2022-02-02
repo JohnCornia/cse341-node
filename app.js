306f0fc750bcdfc5c3da46c8d99bd45e5889d411
John Cornia <john.cornia.1@gmail.com>
2022-02-02 15:33:57 -0700

    up to date

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
 const path = require('path');
 
-const cors = require('cors'); // Place this with other requires (like 'path' and 'express')
-
-const corsOptions = {
-    origin: "https://<your_app_name>.herokuapp.com/",
-    optionsSuccessStatus: 200
-};
-
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://Gmvwt8NmajR6HXR:wTmZTEXhBU6uc8f@cluster0.fgsrg.mongodb.net/shop?retryWrites=true&w=majority";
-
-const PORT = process.env.PORT || 5000
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+/**Keeps Credentials Hidden***/
+//https://www.coderrocketfuel.com/article/store-mongodb-credentials-as-environment-variables-in-nodejs
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MONGODB_URI =
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fgsrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+/*****/
+
 const app = express();
+
+
+/**Here is Heroku integration stuff**/
+const PORT = process.env.PORT || 5000
+
+const cors = require('cors');
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || MONGODB_URI
+
+/*****/
+
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById('61fad515935ec6285d41b21a')
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
             next();
@@ -40,23 +77,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-app.use(cors(corsOptions));
-
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
-};
-
 mongoose
-    .connect(
-        MONGODB_URL, options
-    )
+    .connect(MONGODB_URL, options)
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
