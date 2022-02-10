470b68d584ed9f57d0913c3cce2dcd18a7d1ee4d
John Cornia <john.cornia.1@gmail.com>
2022-02-10 09:38:55 -0700

    third module code

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ dotenv.config();
 const MONGODB_URI =
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fgsrg.mongodb.net/shop?retryWrites=true&w=majority`;
 /*****/
+
 const app = express();
 
 /**Here is Heroku integration stuff**/
@@ -67,31 +68,49 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
+    // throw new Error('Sync Dummy');
     if (!req.session.user) {
         return next();
     }
     User.findById(req.session.user._id)
         .then(user => {
+            if (!user) {
+                return next();
+            }
             req.user = user;
             next();
         })
-        .catch(err => console.log(err));
-});
-
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+        .catch(err => {
+            next(new Error(err));
+        });
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
 
+app.use((error, req, res, next) => {
+    // res.status(error.httpStatusCode).render(...);
+    // res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn
+    });
+});
+
 mongoose
-    .connect(MONGODB_URI)
+    .connect(MONGODB_URL, options)
     .then(result => {
         app.listen(PORT);
     })
